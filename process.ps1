48f6a397867d7ddad39b3efe06d0baf1c8788232
Redouane Oubenal <oubenal@gmail.com>
2019-10-08 00:19:35 +0200

    use processed value

--- process.ps1
+++ process.ps1
@@ -1,5 +1,5 @@
 ﻿$entry = 3,6,4,2,5,2,4,9,6,3,4,4,2,2,2,6
-$max_height = 27
+$max_height = 15
 
 $heap = New-Object 'System.Collections.Generic.List[string]'
 $entry | Sort-Object | foreach { $tmp = $heap.Add($_)}
@@ -22,3 +22,12 @@ do {
         
     if( $heap.Count -eq 0) {break}     
 } while( $true )
+
+
+###############
+##    res:   ##
+#  9 6 6 4 4  #
+#  2 2 2 3 4  #
+#  2 6 5 4    #
+#  2     3    #
+###############
